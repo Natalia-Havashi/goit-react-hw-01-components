cbe561ca97f7efff4ea0ff9737b7a969770b3115
Natalia-Havashi <natali.gavashi@gmail.com>
2023-06-26 17:19:39 +0300

    Update FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './FriendList.module.css';
+import friendsData from './friends.json';
+import { FriendList } from './FriendListItem.jsx';
+import PropTypes from 'prop-types';
+
+
+// console.log(friendsData);
+
+export const Friend = ({friends}) => {
+    
+        return (
+        <ul className="friend-list">
+         
+         <li></li>
+         
+        </ul>
+    ) 
+    
+ 
+}
+
+// Friend.PropTypes = {
+//     avatar: PropTypes.string,
+//     name: PropTypes.string,
+//     id: PropTypes.number,
+//     isOnline: PropTypes.bool,
+// }
